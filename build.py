13794a0b973af5f81d4bba12349c6f1d9353ce10
kan01234 <safghjkl@gmail.com>
2024-07-10 21:20:15 +0900

    add build script

---
+++ build.py
@@ -0,0 +1,20 @@
+from jinja2 import Environment, FileSystemLoader  
+import json
+
+# Open the JSON file in read mode
+with open("data.json", "r") as file:
+  # Load the JSON content into a Python dictionary
+  data = json.load(file)
+
+# Create a Template Loader object
+env = Environment(loader=FileSystemLoader('./template'))
+
+# Load the template
+template = env.get_template('index.html.template')
+
+# Render the template with data
+html_content = template.render(data=data)
+
+# Write rendered HTML to a file (optional)
+with open("index.html", "w") as f:
+    f.write(html_content)
